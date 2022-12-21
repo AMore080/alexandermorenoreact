3d6426d0a69d8b85edb989899a5ded3e2fbf34ec
Alex <alexmore080@gmail.com>
2022-12-20 19:50:30 -0600

    used cloud for my photo

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ class About extends React.Component{
                 <section class="container-lg bg-blue-400 my-20 shadow-2xl">
                     <h2 class="text-5xl text-white p-8 border-solid border-4 border-slate-500 font-bold mx-1 shadow-2xl">About Me</h2>
                         <div class="grid bg-slate-100 flex-row">
-                            <img src="/images/image0.jpeg" alt="a picture of me" class="scale-50 rounded-md z-0 col-span-1" />
+                            <img src="https://res.cloudinary.com/davvalvhs/image/upload/v1671587308/image0_4_fps5u2.jpg" alt="a picture of me" class="scale-50 rounded-md z-0 col-span-1" />
                                 <h1 class="col-span-2 col-start-2 py-24 font-semibold mr-3 lg:text-3xl">I'm Alexander (or Alex for short), I am a learning developer looking pursue a career as a software engineer. I am
                                 a current
                                 student in the Lonestar College system looking to major in Computer Science and am also enrolled in the
