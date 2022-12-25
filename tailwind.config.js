9917a0a4414d0995179612036457c33fa8638c6b
Alex <alexmore080@gmail.com>
2022-12-24 23:25:58 -0600

    custom themes (tailwind)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'deepSeaBlack': '#071013',
+        'coralBlue': '#23B5D3',
+        'greenSea': '#75ABBC',
+        'darkGray': '#A2AEBB',
+        'silverGray': '#DFE0E2'
+      },
+      spacing: {
+        '48r': "48rem"
+      }
+    },
   },
   plugins: [],
 }
