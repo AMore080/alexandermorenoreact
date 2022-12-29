23e60b45951fdba327a06020f0b016d1b622cb10
Alex <alexmore080@gmail.com>
2022-12-28 18:04:02 -0600

    added framer to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from 'framer-motion';
 
 class Header extends React.Component {
     render() {
@@ -7,9 +8,19 @@ class Header extends React.Component {
                 <nav class="w-full flex flex-row bg-coralBlue h-20 items-center sticky top-0 shadow-xl lg:text-3xl sm:text-xs z-20">
                     <ul className="absolute mx-10 decoration-4 text-silverGray"><a href="/">Alex</a></ul>
                     <ul class="w-full flex flex-row font-bold text-white justify-center">
-                        <li class="mx-4 lg:mx-8 underline decoration-4 underline-offset-8 text-silverGray"><a href="/aboutme">About Me</a></li>
-                        <li class="mx-4 lg:mx-8 underline decoration-4 underline-offset-8 text-silverGray"><a href="/projects">Projects</a></li>
-                        <li class="mx-4 lg:mx-8 underline decoration-4 underline-offset-8 text-silverGray"><a href="/contactme">Contact Me</a></li>
+                        <motion.li 
+                        whileHover={{scale: 1.1}}
+                        transition={{type: 'spring', stiffness: 100, damping: 5}}
+                        class="mx-4 lg:mx-8 underline decoration-4 underline-offset-8 text-silverGray"><motion.a
+                        href="/aboutme">About Me</motion.a></motion.li>
+                        <motion.li 
+                        whileHover={{scale: 1.1}}
+                        transition={{type: 'spring', stiffness: 100, damping: 5}}
+                        class="mx-4 lg:mx-8 underline decoration-4 underline-offset-8 text-silverGray"><a href="/projects">Projects</a></motion.li>
+                        <motion.li 
+                        whileHover={{scale: 1.1}}
+                        transition={{type: 'spring', stiffness: 100, damping: 5}}
+                        class="mx-4 lg:mx-8 underline decoration-4 underline-offset-8 text-silverGray"><a href="/contactme">Contact Me</a></motion.li>
                     </ul>
                 </nav>
             </div>
