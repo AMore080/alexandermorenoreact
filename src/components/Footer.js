d4c0b6796c5651045b8a6044cd20b54129c6f2df
Alex <alexmore080@gmail.com>
2022-12-28 18:03:52 -0600

    customized footer with icons and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { Grid } from "@nextui-org/react";
+import githubLogo from '../icons/githubIcon.svg'
+import linkedinLogo from '../icons/linkedinLogo.svg'
+import twitterLogo from '../icons/twitterIcon.svg'
+import emailIcon from '../icons/emailIcon.svg'
 
 class Footer extends React.Component {
     render() {
         return (
             <footer class="bg-greenSea shadow md:px-6 md:py-8">
                 <div class="flex justify-center">
-                    <Grid.Container class="flex flex-wrap mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <Grid.Container class="flex flex-wrap gap-5 mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                         <Grid>
-                            <a href="#" class="mr-4 hover:underline md:mr-6 ">GitHub</a>
+                            <a href="https://github.com/AMore080" class="mr-4 hover:underline md:mr-6 "><img alt="github logo" style={{ height: 63, width: 36 }} src={githubLogo}></img></a>
                         </Grid>
                         <Grid>
-                            <a href="#" class="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                            <a href="https://www.linkedin.com/in/alexander-moreno-9182a6193/" class="mr-4 hover:underline md:mr-6"><img alt="linkedIn logo" style={{ height: 63, width: 36 }} src={linkedinLogo}></img></a>
                         </Grid>
                         <Grid>
-                            <a href="#" class="mr-4 hover:underline md:mr-6 ">Twitter</a>
+                            <a href="https://twitter.com/alexmore080" class="mr-4 hover:underline md:mr-6 "><img alt="twitter logo" style={{ height: 63, width: 36 }} src={twitterLogo}></img></a>
                         </Grid>
                         <Grid>
-                            <a href="#" class="hover:underline">Contact</a>
+                            <a href="alexmore080@gmail.com" class="hover:underline"><img alt="envelope icon" style={{ height: 63, width: 36 }} src={emailIcon}></img></a>
                         </Grid>
                     </Grid.Container>
                 </div>
