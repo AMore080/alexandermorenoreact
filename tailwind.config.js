8f6f37698af323b2fa7cb1ea4ef6b6292303d8a9
AMore080 <alexmore080@gmail.com>
2024-05-13 11:56:42 -0500

    tailwind css addtions

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,16 @@ module.exports = {
         current: 'currentColor',
         'deepSeaBlack': '#071013',
         'coralBlue': '#23B5D3',
-        'greenSea': '#75ABBC',
+        'greenSea': '#1b263b',
         'darkGray': '#A2AEBB',
         'silverGray': '#DFE0E2'
       },
+
+      scale: {
+        '20': '.20',
+        '5': '.5',
+      },
+      
       spacing: {
         '48r': "48rem"
       }
