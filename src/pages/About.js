3631527a50b9233b1aa645c49e63991697fca0c7
Alexander Moreno <alexmore080@gmail.com>
2024-05-31 12:35:29 -0500

    about desc. update

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,16 +20,6 @@ class About extends React.Component{
             }
             }}
             >
-<<<<<<< HEAD
-                <section class=" bg-deepSeaBlack my-10 shadow-2xl flex-col mx-auto w-full md:w-3/4 rounded-lg">
-                    <h2 class="lg:text-5xl p-4 font-bold mx-1 shadow-3xl z-20 text-silverGray flex-1">About Me</h2>
-                        <div class="sm:grid flex-row bg-greenSea items-center rounded-b-lg">
-                            <img src="https://res.cloudinary.com/davvalvhs/image/upload/v1671587308/image0_4_fps5u2.jpg" alt="a picture of me" class="sm:row-auto lg:scale-75 scale-75 rounded-md z-0 col-span-auto float-left p-5" />
-                                <div class="col-span-2 col-start-2 lg:py-28 font-semibold text-silverGray sm:text-xs md:text-lg lg:text-2xl md:p-15 p-5">
-                                    <h1 className="bg-deepSeaBlack mx-5 p-5 rounded-lg">I'm Alexander (or Alex for short), I am a Computer Science student. I have 
-                                    earned my certificate in fullstack web development through the University Of Minesota's bootcamp. I am currently attending the University of Houston
-                                    to complete my Bachelor of Science in Computer Science.
-=======
                 <section class=" bg-deepSeaBlack my-10 shadow-2xl mx-auto w-full box-content md:w-3/4 rounded-lg">
                     <h2 class="lg:text-5xl p-4 font-bold mx-1 z-20 text-silverGray flex-1">About Me</h2>
                         <div className="sm:grid bg-greenSea rounded-b-lg flex flex-col flex-wrap">
@@ -39,9 +29,8 @@ class About extends React.Component{
                                         alt="myself in a red flannel" 
                                         className="lg:scale-75 sm:scale-[.20] max-h-72 mx-auto shrink center border-white border-2 rounded-b-full rounded-t-full"/>
                                     I'm Alexander (or Alex), I am a Computer Science student. I have 
-                                    earned my certificate in fullstack web development through the University Of Minesota's coding bootcamp. I am enrolled at Lone Star College and will transfer to the University of Houston
-                                    to complete my Bachelors of Science in Computer Science.
->>>>>>> 2c9a9503ee5a813e994ac33bf1abb9226a986355
+                                    earned my certificate in fullstack web development through the University Of Minesota's coding bootcamp. I am attending the University of Houston
+                                    to complete my Bachelor of Science in Computer Science.
                                     I plan to continue expanding my skillset and work with a team that I can grow with.
                                     <br />
                                     <br />
